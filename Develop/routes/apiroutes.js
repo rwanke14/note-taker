41c6ef33a6ca31994e49da1a531283d18d33b5e1
Rachel Wanke <rkhorne09@gmail.com>
2021-02-03 01:06:15 -0500

    finishing api routes and cleaning up code

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -6,10 +6,10 @@ const uniqid = require('uniqid');
 
 module.exports = (app) => {
 
-
+    //Gets the note information
     app.get('/api/notes', (req, res) => {
 
-        //fs.readFile(get)
+        //Sends stored notes back to user and they remain till deleted.
         fs.readFile('./Develop/db/db.json', (err, data) => {
             if (err) throw err;
             res.send(data)
@@ -19,21 +19,23 @@ module.exports = (app) => {
     })
 
 
-    //posting to api route
+    //Adding notes to array.
     app.post('/api/notes', (req, res) => {
 
         console.log(req.body)
+        //Setting up object to be inserted into the array in the json file.
         let newNotes = {
             title: req.body.title,
             text: req.body.text,
             id: uniqid()
         }
-
+        //reads json file and pushes new object into array.
         fs.readFile('./Develop/db/db.json', (err, data) => {
             if (err) throw err;
             const parsedData = JSON.parse(data)
             parsedData.push(newNotes);
 
+            //Writes new object into json file and sends back to user.
             fs.writeFile('./Develop/db/db.json', JSON.stringify(parsedData), (err) => {
                 if (err) throw err;
                 console.log('The file has been saved!');
@@ -46,28 +48,28 @@ module.exports = (app) => {
 
     });
 
+    //Deleting note at users request. Identified by it's unique ID. 
     app.delete('/api/notes/:id', (req, res) => {
 
+       //declaring variable for when user selects an id with their request.
         let id = req.params.id
         console.log(id)
 
-        for (let i = 1; i < dbData.length; i++) {
-            console.log('test')
-            console.log(i)
-            if (id == dbData[i].id) {
-                console.log('hello')
-                const update = dbData.splice(id[i], 0)
-                console.log(update)
-                
-                fs.writeFile('./Develop/db/db.json', JSON.stringify(update), (err) => {
-                    if (err) throw err;
-                    console.log('The file has been deleted!');
-                    
-                });
-            }
-        }
-        res.json(dbData);
-    
+
+        //using .pop() instead of .splice() since this could be a large array and it's easier to remove with .pop()
+        
+        dbData.pop(id)
+
+        //Writing the update to the json file to change the array.
+        fs.writeFile('./Develop/db/db.json', JSON.stringify(dbData), (err) => {
+            if (err) throw err;
+            console.log('The file has been deleted!');
+
+        });
+
+        //Sending updated array back to user.
+        res.send(dbData);
+
     });
 
 
--- Develop/routes/htmlroutes.js
+++ Develop/routes/htmlroutes.js
@@ -3,7 +3,11 @@ const path = require('path');
 
 module.exports = (app) => {
 
+//setting up html route to index.html
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+
+//setting up html route to notes.html
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')));
 
